c3078871b8a52192bc567e04ddc7cc43520e4207
SeoHyun Ahn <ahn67@purdue.edu>
2018-05-17 16:59:51 -0400

    Make delete and fav button click change the background as yellow

--- Project2Sub/index.js
+++ Project2Sub/index.js
@@ -12,11 +12,19 @@ const app = {
                 this.handelSubmit(ev)
             })
     },
-    renderListItem(flick){
+    renderListItem(flick) {
         const item = this.template.cloneNode(true)
         item.classList.remove('template')
         item.dataset.id = flick.id
         item.querySelector('.flickName').textContent = flick.name
+        item.querySelector('.alert').addEventListener('click', (ev) => {
+            ev.preventDefault()
+            this.handledelete(ev)
+        })
+        item.querySelector('.button').addEventListener('click', (ev) => {
+            ev.preventDefault()
+            this.handlefav(ev)
+        })
         return item;
     },
 
@@ -25,6 +33,7 @@ const app = {
         const flick = {
             id: ++this.max,
             name: f.flickName.value,
+            fav: false,
         }
         this.flicks.unshift(flick) //역순으로 어레이에 저장 그냥 push면 오는 족족 넣음
 
@@ -34,9 +43,41 @@ const app = {
         f.reset()
     },
 
+    handlefav(ev) {
+        const t = ev.target
+        const li = t.closest('.flick')
+        li.style.background = "yellow"
+        for(let i=0; i < this.flicks.length; i++){
+            if(this.flicks[i].id.toString() == li.dataset.id){
+                this.flicks[i].fav = true
+            }
+        }
+        
+    },
+
+    handledelete(e) {
+        e.preventDefault();
+        const btn = e.target
+        const li = btn.closest('.flick')
+        for(let i=0; i < this.flicks.length; i++){
+            if(this.flicks[i].id.toString() == li.dataset.id){
+                this.flicks.splice(i,1);
+            }
+        }
+        li.remove()
+    },
 }
 app.init({
     formselector: '#flickForm',
     listselector: '#flickList',
     templateSelector: '.flick.template',
 })
+
+// $('ul').on('click', 'button', function(e) {
+
+    // let tempName = $(this).closest('li').eq(0).html();
+    // $(this).closest('li').remove();
+    // console.log(tempName);
+    // let index = globalArr.indexOf(tempName);
+    // globalArr.splice(index, 1);
+//   });
