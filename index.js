368986f1fe33458b90c5a607d18048bb32868c38
SeoHyun Ahn <ahn67@purdue.edu>
2018-05-21 09:46:15 -0400

    remove flicks from the array

--- index.js
+++ index.js
@@ -1,44 +1,76 @@
-const form = document.querySelector('form#userForm')
-var globalArr = new Array;
-const handlesubmit = function(ev) {
-  ev.preventDefault()
-  const addedList = document.querySelector('#addedList')
-  const newAdd = {
-    name: form.teamName.value,
-    date: form.teamDate.value,
-  }
-  globalArr.push(newAdd)
-  addedList.appendChild(renderList(newAdd))
-  form.reset()
-  form.teamName.focus()
-}
-form.addEventListener('submit', handlesubmit)
+const app = {
+    max: 0,
+    init(selectors) {
+        this.flicks = []
+        this.max = 0
+        this.list = document.querySelector(selectors.listselector)
+        this.template = document.querySelector(selectors.templateSelector)
+        document
+            .querySelector(selectors.formselector)
+            .addEventListener('submit', (ev) => {
+                ev.preventDefault()
+                this.handelSubmit(ev)
+            })
+    },
+    renderListItem(flick) {
+        const item = this.template.cloneNode(true)
+        item.classList.remove('template')
+        item.dataset.id = flick.id
+        item.querySelector('.flickName').textContent = flick.name
+        item.querySelector('.remove.button').addEventListener('click', this.handledelete.bind(this, flick))
+        item.querySelector('.button').addEventListener('click', (ev) => {
+            ev.preventDefault()
+            this.handlefav(ev)
+        })
+        return item;
+    },
 
-function renderList(newAdd) {
-  const ul = document.createElement('tr');
-  Object.keys(newAdd).map(key => {
-    const item = renderListIterm(key, newAdd[key])
-    ul.appendChild(item)
-  })
-  const button = document.createElement('button')
-  button.textContent = 'delete'
-  button.class = "delete"
-  ul.appendChild(button)
-  return ul
-}
+    handelSubmit(ev) {
+        const f = ev.target
+        const flick = {
+            id: ++this.max,
+            name: f.flickName.value,
+            fav: false,
+        }
+        this.flicks.unshift(flick) //역순으로 어레이에 저장 그냥 push면 오는 족족 넣음
 
-function renderListIterm(ul, items) {
-  const li = document.createElement('td')
-  li.textContent += `${items}`
-  // localStorage.setItem(ul, items)
-  return li;
+        const item = this.renderListItem(flick)
+        this.list.insertBefore(item, this.list.firstElementChild)
+        console.log(flick);
+        f.reset()
+    },
+
+    handlefav(ev) {
+        const t = ev.target
+        const li = t.closest('.flick')
+        li.style.background = "yellow"
+        for(let i=0; i < this.flicks.length; i++){
+            if(this.flicks[i].id.toString() == li.dataset.id){
+                this.flicks[i].fav = true
+            }
+        }
+        
+    },
+
+    handledelete(flick, e) {
+        e.preventDefault();
+        const btn = e.target
+        const li = btn.closest('.flick')
+        //Can use ev.target.parentNode.parentNode but it will break if not li
+        //So we can use cloest(li) but as we have an id for li, we can write with flick
+        // for(let i=0; i < this.flicks.length; i++){
+        //     if(this.flicks[i].id.toString() == li.dataset.id){
+        //         this.flicks.splice(i,1);
+        //     }
+        // }
+        const t = this.flicks.indexOf(flick)
+        this.flicks.splice(t,1);
+        //loop through array 
+        li.remove()
+    },
 }
-$('table').on('click', 'button', function(e) {
-  e.preventDefault();
-  let tempName = $(this).closest('tr').find('td').eq(0).html();
-  $(this).closest('tr').remove();
-  console.log(tempName);
-  let index = globalArr.indexOf(tempName);
-  // localStorage.removeItem(tempName)
-  globalArr.splice(index, 1);
-});
+app.init({
+    formselector: '#flickForm',
+    listselector: '#flickList',
+    templateSelector: '.flick.template',
+})
